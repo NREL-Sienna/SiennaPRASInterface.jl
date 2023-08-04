32f1c02fe7988b321ab16055520478703978bf1f
sdhulipa <suryachandan.dhulipala@nrel.gov>
2023-08-04 14:11:13 -0600

    PSY2PRAS only looked for PowerLoads, temporary fix for PR100

--- src/main/PSY2PRAS.jl
+++ src/main/PSY2PRAS.jl
@@ -57,7 +57,7 @@ end
 #######################################################
 # Function to get available components in AggregationTopology
 #######################################################
-function get_available_components_in_aggregation_topology(type::Type{<:PowerSystems.StaticInjection}, sys::PSY.System, region::PSY.AggregationTopology)
+function get_available_components_in_aggregation_topology(type::Type{<:PSY.StaticInjection}, sys::PSY.System, region::PSY.AggregationTopology)
     avail_comps =  [comp for comp in PSY.get_components_in_aggregation_topology(type, sys, region) if (PSY.get_available(comp))]
 
     return avail_comps
@@ -386,8 +386,8 @@ function make_pras_system(sys::PSY.System;
     region_load = Array{Int64,2}(undef,num_regions,N);
    
     for (idx,region) in enumerate(regions)
-        reg_load_comps = availability_flag ? get_available_components_in_aggregation_topology(PSY.PowerLoad, sys, region) :
-                                             PSY.get_components_in_aggregation_topology(PSY.PowerLoad, sys, region)
+        reg_load_comps = availability_flag ? get_available_components_in_aggregation_topology(PSY.StandardLoad, sys, region) :
+                                             PSY.get_components_in_aggregation_topology(PSY.StandardLoad, sys, region)
         if (length(reg_load_comps) > 0)
             region_load[idx,:]=floor.(Int,sum(get_forecast_values.(first.(PSY.get_time_series_multiple.(reg_load_comps, name = "max_active_power")))
                             .*PSY.get_max_active_power.(reg_load_comps))); # Any issues with using the first of time_series_multiple?
