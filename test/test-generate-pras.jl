5db70e491b7b19be6efe64d2a9c0d33a7e41a4ab
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2024-11-11 17:01:54 -0700

    Add default data test and add max capacity broken test

--- test/test-generate-pras.jl
+++ test/test-generate-pras.jl
@@ -99,13 +99,42 @@ end
         TimeSeries.timestamp(psy_ts.data) .== collect(DateTime.(rts_pras_sys.timestamps)),
     )
 
-    rts_pras_sys =
-        generate_pras_system(rts_da_sys, PSY.Area, lump_region_renewable_gens=true)
-    @test rts_pras_sys isa PRASInterface.PRAS.SystemModel
-    @test test_names_equal(rts_pras_sys.regions.names, area_names)
-    println(rts_pras_sys.timestamps)
+    # Test capacities
+    # 201_HYDRO_4 should have max active power from time series
+    idx = findfirst(x -> x == "201_HYDRO_4", rts_pras_sys.generators.names)
+    hydro_component = PSY.get_component(PSY.HydroDispatch, rts_da_sys, "201_HYDRO_4")
+    max_capacity =
+        PSY.get_time_series_values(
+            PSY.SingleTimeSeries,
+            hydro_component,
+            "max_active_power",
+        ) .* PSY.get_max_active_power(hydro_component)
+    @test isapprox(rts_pras_sys.generators.capacity[idx, 1], max_capacity[1]) broken = true
+    @test all(isapprox.(rts_pras_sys.generators.capacity[idx, :], max_capacity)) broken =
+        true
+
+    # Test Assess Run
+    sequential_monte_carlo = PRASInterface.PRAS.SequentialMonteCarlo(samples=2)
+    shortfalls, = PRASInterface.PRAS.assess(
+        rts_pras_sys,
+        sequential_monte_carlo,
+        PRASInterface.PRAS.Shortfall(),
+    )
+    lole = PRASInterface.PRAS.LOLE(shortfalls)
+    eue = PRASInterface.PRAS.EUE(shortfalls)
+    @test lole isa PRASInterface.PRAS.ReliabilityMetric
+    @test eue isa PRASInterface.PRAS.ReliabilityMetric
+    @test PRASInterface.PRAS.val(lole) >= 0 && PRASInterface.PRAS.val(lole) <= 10
+    @test PRASInterface.PRAS.stderror(lole) >= 0 && PRASInterface.PRAS.stderror(lole) <= 10
+    @test PRASInterface.PRAS.val(eue) >= 0 && PRASInterface.PRAS.val(eue) <= 10
+    @test PRASInterface.PRAS.stderror(eue) >= 0 && PRASInterface.PRAS.stderror(eue) <= 10
 
     @testset "Lumped Renewable Generators" begin
+        rts_pras_sys =
+            generate_pras_system(rts_da_sys, PSY.Area, lump_region_renewable_gens=true)
+        @test rts_pras_sys isa PRASInterface.PRAS.SystemModel
+        @test test_names_equal(rts_pras_sys.regions.names, area_names)
+
         rts_pras_sys = generate_pras_system(
             rts_da_sys,
             PSY.Area,
@@ -129,6 +158,16 @@ end
     end
 end
 
+function array_all_equal(x::AbstractVector{T}, v::T) where {T}
+    for (i, xi) in enumerate(x)
+        if !isapprox(xi, v)
+            @error "array[$i]: $xi != $v"
+            return false
+        end
+    end
+    return true
+end
+
 @testset "RTS GMLC with default data" begin
     rts_da_sys = PSCB.build_system(PSCB.PSISystems, "RTS_GMLC_DA_sys")
     area_names = PSY.get_name.(PSY.get_components(PSY.Area, rts_da_sys))
@@ -147,4 +186,9 @@ end
     @test test_names_equal(rts_pras_sys.regions.names, area_names)
     @test test_names_equal(rts_pras_sys.generators.names, generator_names)
     @test test_names_equal(rts_pras_sys.storages.names, storage_names)
+    # 201_HYDRO_4 should have 5.15 FOR, 11.6 POR, and 22 MTTR
+    idx = findfirst(x -> x == "201_HYDRO_4", rts_pras_sys.generators.names)
+    λ, μ = PRASInterface.rate_to_probability(5.15, 22)
+    @test array_all_equal(rts_pras_sys.generators.λ[idx, :], λ)
+    @test array_all_equal(rts_pras_sys.generators.μ[idx, :], μ)
 end
