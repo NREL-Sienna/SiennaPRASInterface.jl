579d219ca46ded7edb578f9b8fa9674a057f99c2
Dhulipala <sdhulipa@nrel.gov>
2024-12-24 15:14:28 -0700

    Add tests for sub-hourly systems and fix PRAS parameters when making Lines

--- test/test-assess-system.jl
+++ test/test-assess-system.jl
@@ -1,9 +1,32 @@
-@testset "test assess(::PSY.System, ::Area, ...)" begin
-    rts_da_sys = get_rts_gmlc_outage()
+@testset "test assess(::PSY.System, ::Area, ...) Hourly PRAS System" begin
+    rts_da_sys = get_rts_gmlc_outage("DA")
 
     sequential_monte_carlo = SiennaPRASInterface.SequentialMonteCarlo(samples=2, seed=1)
     shortfalls, = SiennaPRASInterface.assess(
         rts_da_sys,
+        PSY.Area,
+        sequential_monte_carlo,
+        SiennaPRASInterface.Shortfall(),
+    )
+    @test shortfalls isa SiennaPRASInterface.PRASCore.Results.ShortfallResult
+
+    lole = SiennaPRASInterface.LOLE(shortfalls)
+    eue = SiennaPRASInterface.EUE(shortfalls)
+    @test lole isa SiennaPRASInterface.PRASCore.ReliabilityMetric
+    @test eue isa SiennaPRASInterface.PRASCore.ReliabilityMetric
+    @test SiennaPRASInterface.val(lole) >= 0 && SiennaPRASInterface.val(lole) <= 10
+    @test SiennaPRASInterface.stderror(lole) >= 0 &&
+          SiennaPRASInterface.stderror(lole) <= 10
+    @test SiennaPRASInterface.val(eue) >= 0 && SiennaPRASInterface.val(eue) <= 10
+    @test SiennaPRASInterface.stderror(eue) >= 0 && SiennaPRASInterface.stderror(eue) <= 10
+end
+
+@testset "test assess(::PSY.System, ::Area, ...) Sub-Hourly System" begin
+    rts_rt_sys = get_rts_gmlc_outage("RT")
+
+    sequential_monte_carlo = SiennaPRASInterface.SequentialMonteCarlo(samples=2, seed=1)
+    shortfalls, = SiennaPRASInterface.assess(
+        rts_rt_sys,
         PSY.Area,
         sequential_monte_carlo,
         SiennaPRASInterface.Shortfall(),
