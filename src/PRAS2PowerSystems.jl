d0b35a4f1cb94d7d793c4ccea426e5d206da448d
Dhulipala <sdhulipa@nrel.gov>
2025-02-25 17:27:23 -0700

    Simplify the logic of PRAS2PSY

--- src/PRAS2PowerSystems.jl
+++ src/PRAS2PowerSystems.jl
@@ -20,9 +20,8 @@ function generate_outage_profile!(
     method::PRASCore.SequentialMonteCarlo,
 )
     pras_system = generate_pras_system(sys, template)
-    resultsspecs = get_outage_pras_resultspec(template)
-    results = PRASCore.assess(pras_system, method, resultsspecs...)
-    add_asset_status!(sys, SPIOutageResult(results), template, resultsspecs)
+    results = PRASCore.assess(pras_system, method, OUTAGE_RESULT_SPEC...)
+    add_asset_status!(sys, SPIOutageResult(results), template)
     return sys
 end
 
@@ -86,20 +85,14 @@ Add the asset status from the worst sample to PSY.TimeSeriesForcedOutage of the 
 # Arguments
 
   - `sys::PSY.System`: PowerSystems.jl system model
-  - `results::T`: Tuple of results from the PRAS assess call
+  - `results::T`: SPIOutageResult
   - `template::RATemplate`: PRAS problem template
-  - `resultsspecs::Vector{PRASCore.Results.ResultSpec}`: PRAS ResultSpecs
   
 # Returns
 
     - PSY System with PSY.TimeSeriesForcedOutage for all components for which asset status is available
 """
-function add_asset_status!(
-    sys::PSY.System,
-    results::SPIOutageResult,
-    template::RATemplate,
-    resultsspecs::Vector{PRASCore.Results.ResultSpec},
-)
+function add_asset_status!(sys::PSY.System, results::SPIOutageResult, template::RATemplate)
     # Time series timestamps
     all_ts = PSY.get_time_series_multiple(sys, x -> (typeof(x) <: PSY.StaticTimeSeries))
     ts_timestamps = TimeSeries.timestamp(first(all_ts).data)
@@ -108,37 +101,32 @@ function add_asset_status!(
 
     sample_idx = argmax(shortfall_samples[])
 
-    num_fields = fieldcount(SPIOutageResult)
-    for i in 2:num_fields
-        fname = fieldname(SPIOutageResult, i)
-        ftype = fieldtype(SPIOutageResult, i)
-        if !(isnothing(getproperty(results, fname)))
-            device_ramodel = get_device_ramodel(ftype)
-            gens_to_formula = build_component_to_formulation(
-                device_ramodel.model,
-                sys,
-                template.device_models,
-            )
+    for result in
+        [results.gen_availability, results.stor_availability, results.gen_stor_availability]
+        device_ramodel = get_device_ramodel(typeof(result))
+        gens_to_formula = build_component_to_formulation(
+            device_ramodel.model,
+            sys,
+            template.device_models,
+        )
 
-            gens_avail = getproperty(results, fname)
+        for gen in keys(gens_to_formula)
+            pras_gen_names = getfield(result, device_ramodel.key)
+            if (gen.name in pras_gen_names)
+                ts_forced_outage = PSY.TimeSeriesForcedOutage(;
+                    outage_status_scenario="WorstShortfallSample",
+                )
+                PSY.add_supplemental_attribute!(sys, gen, ts_forced_outage)
 
-            for gen in keys(gens_to_formula)
-                if (gen.name in getproperty(gens_avail, device_ramodel.key))
-                    ts_forced_outage = PSY.TimeSeriesForcedOutage(;
-                        outage_status_scenario="WorstShortfallSample",
-                    )
-                    PSY.add_supplemental_attribute!(sys, gen, ts_forced_outage)
+                availability_data = TimeSeries.TimeArray(
+                    ts_timestamps,
+                    getindex.(result[gen.name, :], sample_idx),
+                )
+                availability_timeseries =
+                    PSY.SingleTimeSeries("availability", availability_data)
 
-                    availability_data = TimeSeries.TimeArray(
-                        ts_timestamps,
-                        getindex.(gens_avail[gen.name, :], sample_idx),
-                    )
-                    availability_timeseries =
-                        PSY.SingleTimeSeries("availability", availability_data)
-
-                    PSY.add_time_series!(sys, ts_forced_outage, availability_timeseries)
-                    @info "Added availability time series to TimeSeriesForcedOutage supplemental attribute of $(gen.name)."
-                end
+                PSY.add_time_series!(sys, ts_forced_outage, availability_timeseries)
+                @info "Added availability time series to TimeSeriesForcedOutage supplemental attribute of $(gen.name)."
             end
         end
     end
--- src/util/parsing/result_export_helper_functions.jl
+++ src/util/parsing/result_export_helper_functions.jl
@@ -1,69 +1,16 @@
-"""
-    get_outage_pras_resultspec(
-        template::RATemplate,
-    )
-
-Get ResultSpec to be used by PRAS.assess() to get asset availability based on the 
-template.
-
-# Arguments
-
-  - `template::RATemplate`: PRAS problem template
-
-# Returns
-
-  - PRAS ResultSpec
-"""
-
-function get_outage_pras_resultspec(template::RATemplate)
-    # We have to change this once we define a formulation for Lines
-    resultsspecs = PRASCore.Results.ResultSpec[]
-    push!(resultsspecs, ShortfallSamples())
-
-    for model in template.device_models
-        result_spec = get_pras_resultspec(typeof(get_formulation(model)))
-        if !(result_spec in resultsspecs)
-            push!(resultsspecs, result_spec)
-        end
-    end
-
-    return resultsspecs
-end
-
-"""
-Get PRAS ResultSpec for RAFormulation
-"""
-function get_pras_resultspec(::Type{GeneratorPRAS})
-    return GeneratorAvailability()
-end
-
-"""
-Get PRAS ResultSpec for RAFormulation
-"""
-function get_pras_resultspec(::Type{B}) where {B <: StoragePRAS}
-    return StorageAvailability()
-end
-
-"""
-Get PRAS ResultSpec for RAFormulation
-"""
-function get_pras_resultspec(::Type{B}) where {B <: GeneratorStoragePRAS}
-    return GeneratorStorageAvailability()
-end
-
-"""
-Get PRAS ResultSpec for AbstractRAFormulation
-"""
-function get_pras_resultspec(::Type{B}) where {B <: AbstractRAFormulation}
-    return error("PRAS ResultSpec not defined for $(B)")
-end
+const OUTAGE_RESULT_SPEC = PRASCore.Results.ResultSpec[
+    ShortfallSamples(),
+    GeneratorAvailability(),
+    StorageAvailability(),
+    GeneratorStorageAvailability(),
+]
 
 """
 Get DeviceRAModel for PRAS AbstractAvailabilityResult
 """
 function get_device_ramodel(
-    ::Type{Union{Nothing, PRASCore.Results.GeneratorAvailabilityResult}},
-)
+    ::Type{T},
+) where {T <: PRASCore.Results.GeneratorAvailabilityResult}
     return (model=GeneratorPRAS, key=:generators)
 end
 
@@ -71,8 +18,8 @@ end
 Get DeviceRAModel for PRAS AbstractAvailabilityResult
 """
 function get_device_ramodel(
-    ::Type{Union{Nothing, PRASCore.Results.StorageAvailabilityResult}},
-)
+    ::Type{T},
+) where {T <: PRASCore.Results.StorageAvailabilityResult}
     return (model=StoragePRAS, key=:storages)
 end
 
@@ -80,37 +27,9 @@ end
 Get DeviceRAModel for PRAS AbstractAvailabilityResult
 """
 function get_device_ramodel(
-    ::Type{Union{Nothing, PRASCore.Results.GeneratorStorageAvailabilityResult}},
-)
+    ::Type{T},
+) where {T <: PRASCore.Results.GeneratorStorageAvailabilityResult}
     return (model=GeneratorStoragePRAS, key=:generatorstorages)
-end
-
-"""
-Get SPIOutageResult fname for PRAS ResultSpec
-"""
-function get_outage_result_fname(::Type{GeneratorAvailability})
-    return :gen_availability
-end
-
-"""
-Get SPIOutageResult fname for PRAS ResultSpec
-"""
-function get_outage_result_fname(::Type{StorageAvailability})
-    return :stor_availability
-end
-
-"""
-Get SPIOutageResult fname for PRAS ResultSpec
-"""
-function get_outage_result_fname(::Type{GeneratorStorageAvailability})
-    return :gen_stor_availability
-end
-
-"""
-Get SPIOutageResult fname for PRAS ResultSpec
-"""
-function get_outage_result_fname(::Type{R}) where {R <: PRASCore.Results.ResultSpec}
-    return error("SPIOutageResult currently is not defined for $(R)")
 end
 
 """
@@ -121,47 +40,17 @@ $(TYPEDFIELDS)
 
 SPIOutageResult is used to parse Tuple{Vararg{PRAS.PRASCore.Results.Result}} and add structure to it.
 """
-mutable struct SPIOutageResult
+struct SPIOutageResult
     "Shortfall Sample Result"
-    shortfall_samples::Union{Nothing, PRASCore.Results.ShortfallSamplesResult}
+    shortfall_samples::PRASCore.Results.ShortfallSamplesResult
     "Generator Availability Result"
-    gen_availability::Union{Nothing, PRASCore.Results.GeneratorAvailabilityResult}
+    gen_availability::PRASCore.Results.GeneratorAvailabilityResult
     "Storage Availability Result"
-    stor_availability::Union{Nothing, PRASCore.Results.StorageAvailabilityResult}
+    stor_availability::PRASCore.Results.StorageAvailabilityResult
     "GeneratorStorage Availability Result"
-    gen_stor_availability::Union{
-        Nothing,
-        PRASCore.Results.GeneratorStorageAvailabilityResult,
-    }
-
-    function SPIOutageResult(;
-        shortfall_samples=nothing,
-        gen_availability=nothing,
-        stor_availability=nothing,
-        gen_stor_availability=nothing,
-    )
-        return new(
-            shortfall_samples,
-            gen_availability,
-            stor_availability,
-            gen_stor_availability,
-        )
-    end
+    gen_stor_availability::PRASCore.Results.GeneratorStorageAvailabilityResult
 end
 
 function SPIOutageResult(results::T) where {T <: Tuple{Vararg{PRASCore.Results.Result}}}
-    num_fields = fieldcount(SPIOutageResult)
-    outage_result = SPIOutageResult()
-    for i in 1:num_fields
-        fname = fieldname(SPIOutageResult, i)
-        ftype = fieldtype(SPIOutageResult, i)
-        if ~(isempty(filter(x -> typeof(x) <: ftype, results)))
-            setproperty!(
-                outage_result,
-                fname,
-                first(filter(x -> typeof(x) <: ftype, results)),
-            )
-        end
-    end
-    return outage_result
+    SPIOutageResult(results...)
 end
